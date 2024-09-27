896eeb842b210442abe8e3e763c184af4f92b32b
douni05 <douni05@gmail.com>
2024-09-27 18:42:42 +0900

    v0.3

--- week05/main.go
+++ week05/main.go
@@ -1,18 +1,28 @@
 package main
 
-import "fmt" // c 언어 #include <stdio.h>
+import (
+	"fmt" // c 언어 #include <stdio.h>
+	"math"
+	"reflect"
+	"strings"
+)
 
 func main() {
 	//var i int
 	//i = 55
 
 	//var i int = 55
-	var f float32 = 1.92
+	var f float64 = 1.92
 
-	i := 55
+	//f := 1.92
+	i := "55"
+	fmt.Println(strings.Title("kim doyun"))
+	fmt.Printf("%f %f\n", f, math.Ceil(f))
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
 	fmt.Println("f is", f)
 	fmt.Println("i is", i)
 	fmt.Print("i is ", i, "\n")
 	fmt.Println("i is", i)
-	fmt.Printf("i is %d\n", i)
+	//fmt.Printf("i is %d\n", i)
+	fmt.Printf("i is %s\n", i)
 }
