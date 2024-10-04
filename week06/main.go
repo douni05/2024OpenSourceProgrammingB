7f85d180b5b11f91b63b537daff075b603a4d409
douni05 <douni05@gmail.com>
2024-10-04 17:55:05 +0900

    v0.5 Unicode

--- week06/main.go
+++ week06/main.go
@@ -8,10 +8,14 @@ import (
 func main() {
 	i := 13
 	var f float64 = 12.9 // f := 12.9
+	c1 := 'Z'
+	c2 := '김'
 
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f)  // invalid operation: i * f (mismatched types int and float64) : go에서 묵시적 형변환 X
 	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Println(c1, c2)
+	fmt.Printf("%x\n", c2) // 유니코드 '감'에 대한 16진수 출력, home.unicode.org
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
