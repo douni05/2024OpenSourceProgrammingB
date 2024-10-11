dd5e86c0f1b5d355eee92cf2a1330feede381b59
douni05 <douni05@gmail.com>
2024-10-11 18:26:06 +0900

    v1.3 PraseInt, Base16

--- week07/main.go
+++ week07/main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	var realscore float64
 	fmt.Print("점수 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	score, err := in.ReadString('\n')
@@ -19,11 +18,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	score = strings.TrimSpace(score)             // 줄바꿈, 띄어쓰기, 탭 줄 제거 (python, strip 과 유사)
-	realscore, _ = strconv.ParseFloat(score, 64) // 실수형 64비트 다입으로 형변환
-	if realscore >= 90 {
+	score = strings.TrimSpace(score)                // 줄바꿈, 띄어쓰기, 탭 줄 제거 (python, strip 과 유사)
+	realscore, _ := strconv.ParseInt(score, 16, 32) // 정수형 32비트 다입으로 형변환
+	if realscore >= 60 {
 		fmt.Println("A")
+		fmt.Printf("%d\n", realscore)
 	} else {
 		fmt.Println("BCDE")
+		fmt.Printf("%d\n", realscore)
 	}
 }
