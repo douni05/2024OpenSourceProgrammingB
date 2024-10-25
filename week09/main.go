e33a2589df8e5ae10c2168c40a63bd7bcd025a22
douni05 <douni05@gmail.com>
2024-10-25 16:53:44 +0900

    v1.6 Guess games

--- week09/main.go
+++ week09/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,4 +15,18 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	answer := rand.Intn(6) + 1 // dice 1 ~ 6
 	fmt.Println(answer)
+
+	fmt.Print("숫자 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	input, err := in.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input) // 줄바꿈, 띄어쓰기, 탭 줄 제거 (python, strip 과 유사)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(guess)
 }
