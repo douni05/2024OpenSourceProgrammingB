8d7f83ead7f9616ab21e5bfc5aedd5663fc6bd3e
douni05 <douni05@gmail.com>
2024-11-29 16:26:09 +0900

    v4.5

--- week12/main.go
+++ week12/main.go
@@ -6,16 +6,21 @@ import (
 )
 
 func main() {
-	
-	gpaSlice := []float64{4.1, 4.5, 3.9} // slice literal
+	// Create a slice by make function
+	var gpaSlice []float64
+	gpaSlice = make([]float64, 3)
+	gpaSlice[0] = 4.1
+	gpaSlice[1] = 4.5
+	gpaSlice[2] = 3.9
 	fmt.Println(gpaSlice, reflect.TypeOf(gpaSlice))
 
+	// Create a slice by slice literal
 	// gpaSlice := []float64{4.1, 4.5, 3.9} // slice literal
 	// fmt.Println(gpaSlice, reflect.TypeOf(gpaSlice))
 
-	// 
-	// 	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
-	// 	fmt.Println(gpas, reflect.TypeOf(gpas))
-	// 	gpaSlice := gpas[1:4] // slice := slicing array
-	// 	fmt.Println(gpaSlice, reflect.TypeOf(gpaSlice))
+	// Create a slice by slicing an existing array
+	// gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
+	// fmt.Println(gpas, reflect.TypeOf(gpas))
+	// gpaSlice := gpas[1:4] // slice := slicing array
+	// fmt.Println(gpaSlice, reflect.TypeOf(gpaSlice))
 }
--- week13/main.go
+++ week13/main.go
@@ -5,19 +5,5 @@ import (
 )
 
 func main() {
-	var emptySlice []bool
-	//emptySlice = make([]bool, 5)
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // slice zero value (nil), 0
-	if len(emptySlice) == 0 {
-		emptySlice = append(emptySlice, true)
-	}
-	fmt.Printf("%#v %d\n", emptySlice, len(emptySlice)) // []bool{true}, 1
-
-	gpas := [5]float64{3.5, 4.1, 4.5, 3.9, 4.23} // array := array literal
-	gpa_slice := gpas[1:4]                       // [4.1, 4.5, 3.9]
-	//gpa_slice[1] = 2.71
-	gpas[2] = 2.71
-	//gpa_slice = append(gpa_slice, 4.3)
-	gpa_slice = append(gpa_slice, 4.3, 5.55)
-	fmt.Println(len(gpa_slice), gpa_slice, gpas)
+	
 }
